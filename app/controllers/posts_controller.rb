492e487a9208a46d666a6d743ec48f0ed58a9e08
lotus0099 <lvdeshi2011@gmail.com>
2017-11-21 17:54:01 +0800

    add like&unlike function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,22 @@ class PostsController < ApplicationController
         @post = current_user.posts.find(params[:id])
         @post.destroy
     end
+    def like
+    @post = Post.find(params[:id])
+    unless @post.find_like(current_user)  # 如果已经按讚过了，就略过不再新增
+      Like.create( :user => current_user, :post => @post)
+    end
 
+
+  end
+
+  def unlike
+    @post = Post.find(params[:id])
+    like = @post.find_like(current_user)
+    like.destroy
+    render "like"
+
+  end
     protected
     def post_params
         params.require(:post).permit(:content)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :posts
-         def display_name
+    def display_name
      # # 取 email 的前半来显示，如果你也可以另开一个字段是 nickname 让用户可以自己编辑显示名称
      self.email.split("@").first
    end
+    has_many :likes,dependent: :destroy
+    has_many :liked_posts, through: :likes,source: :posts
 end
