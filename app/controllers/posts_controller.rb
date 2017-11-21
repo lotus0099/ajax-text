1fdfa8d7568933cbb64e705271b320aa6b2c80d9
lotus0099 <lvdeshi2011@gmail.com>
2017-11-21 17:01:18 +0800

    make form ajax create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,19 +3,14 @@ class PostsController < ApplicationController
     def index
         @posts = Post.order("id DESC").all    # 新贴文放前面
     end
-
     def create
         @post = Post.new(post_params)
         @post.user = current_user
         @post.save
-        redirect_to posts_path
-
     end
     def destroy
         @post = current_user.posts.find(params[:id])
         @post.destroy
-        
-
     end
 
     protected
